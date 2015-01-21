f46f50f5a06c28ef59b0acde4cafc0446dd495fe
Andrew Leonard <andy.j.leonard@gmail.com>
2015-01-21 16:12:38 +0000

    More various plotting things added

--- plotflares3.py
+++ plotflares3.py
@@ -9,6 +9,8 @@ from matplotlib import use
 use('agg')
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cmx
+import matplotlib.colors as colours
 from sunpy import wcs
 from sunpy.net import hek
 from sunpy.time import parse_time as parse
@@ -73,9 +75,20 @@ ar_temps_10 = []
 ar_temps_30 = []
 fl_classes = []
 
-flarelist = open("flarelist.txt", w)
+flarelist = open("/imaps/holly/home/ajl7/tempplots/flarelist.txt", "w")
 
-fig = plt.figure()
+fig = plt.figure(figsize=(24, 8))
+ax1 = fig.add_subplot(1, 3, 1)
+ax1.set_title('Temperature value over time')
+ax2 = fig.add_subplot(1, 3, 2)
+ax2.set_title('Running difference')
+ax3 = fig.add_subplot(1, 3, 3)
+ax3.set_title('Difference from temperature at flare time')
+
+# Set up some colourmap stuff for line-plotting later
+cmap = cm = plt.get_cmap('afmhot')
+cNorm  = colours.Normalize(vmin=0, vmax=1)
+scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cmap)
 
 for flare in flares:
   try:
@@ -83,7 +96,7 @@ for flare in flares:
     starttime = flaretime-dt.timedelta(hours=0.5)
     timerange = tr(starttime, flaretime)
 
-    flarelist.write(flaretime.date, flaretime.time, "&", flare['fl_goescls'], "&", flare['ar_noaanum'], "\\\\")
+    flarelist.write("{} {} & {} & {} \\\\ \n".format(flaretime.date(), flaretime.time(), flare['fl_goescls'], flare['ar_noaanum']))
     region = client.query(hek.attrs.EventType('AR'),
                           hek.attrs.Time(flaretime-dt.timedelta(minutes=5), 
                                          flaretime))
@@ -109,7 +122,11 @@ for flare in flares:
     data_dir = '/imaps/sspfs/archive/sdo/aia/activeregions/AR{}/data/'.format(flare['ar_noaanum'])
     maps_root = '/imaps/sspfs/archive/sdo/aia/fulldisk/images/' #'/imaps/holly/home/ajl7/tempmaps'
     
+    # Create empty lists to store temperature values and running differences
+    # Called means because I did mean first, but used for max, percentiles,
+    # etc sometimes
     means = []
+    runningdiffT = []
     for time in times:
         # Load/calculate temperature map data
         print time
@@ -126,16 +143,29 @@ for flare in flares:
             
             # Append appropriate temperature values to list
             means.append(np.nanmean(thismap.data))
+            #means.append(np.nanmax(thismap.data))
+            #means.append(np.percentile(thismap.data, 95))
+            if len(means) > 1:
+                runningdiffT.append(means[-1]-means[-2])
+            else:
+                runningdiffT.append(0)
         except:
             print "Failed", time
             means.append(np.nan)
             #raise
     
+    print means
     # Convert time values to time before flare
     times = [(t - flaretime).total_seconds()/60 for t in times]
+    # Decide line colour based on flare class
+    flarecolours = {'A': 0.3, 'B': 0.4, 'C': 0.5, 'M': 0.6, 'X': 0.7}
+    colourVal = scalarMap.to_rgba(flarecolours[str(flare['fl_goescls'])[0].upper()])
     # Plot temperature values of AR with time for that flare
     ##fig = plt.figure()
-    plt.plot(times, means)
+    ax1.plot(times, means, color=colourVal)
+    ax2.plot(times, runningdiffT, color=colourVal)
+    ax3.plot(times, [mean-means[-1] for mean in means], color=colourVal) # Absolute difference
+    #ax3.plot(times, [((mean-means[-1])/means[-1])*100 for mean in means], color=colorVal) # Percentage difference
     ##fname = 'AR{}/{}__{}-class'.format(flare['ar_noaanum'], flare['event_starttime'], flare['fl_goescls'])
     ##fname = fname.replace('.', '_')
     ##plt.savefig(join('/imaps/holly/home/ajl7/tempplots/', fname))
@@ -146,6 +176,7 @@ for flare in flares:
     ar_temps_10.append(means[-11])
     ar_temps_30.append(means[0])
     # Append class of flare to list
+    #fl_classes.append(flareclass_to_flux(str(flare['fl_goescls'])).value)
     fl_classes.append(np.log10(flareclass_to_flux(str(flare['fl_goescls'])).value))
   except:
     print 'Failed for {} flare at {}'.format(flare['fl_goescls'], flare['event_starttime'])
@@ -153,27 +184,63 @@ for flare in flares:
 
 flarelist.close()
 
-plt.ylim(5.9, 6.3)
+#ax1.ylim(5.9, 6.3)
 plt.savefig("/imaps/holly/home/ajl7/tempplots/allars")
 plt.close()
+
+"""# Plot ratio of temperatures to given temperature over time
+fig = plt.figure(figsize=(10, 10))
+ax1 = fig.add_subplot(2, 2, 1)
+plt.title("T(t)/T(t=x)")
+ratio = [mean/means[-1] for mean in means]
+print min(ratio), max(ratio)
+plt.plot(times, [mean/means[-1] for mean in means], label='x = 0')
+plt.ylabel("Temperature ratio")
+ax2 = fig.add_subplot(2, 2, 2)
+plt.title("T(t)/T(t=-1)")
+ratio =	[mean/means[-2] for mean in means]
+print min(ratio), max(ratio)
+plt.plot(times, [mean/means[-2] for mean in means], label='x = -1')
+ax3 = fig.add_subplot(2, 2, 3)
+plt.title("T(t)/T(t=-10)")
+ratio =	[mean/means[-11] for mean in means]
+print min(ratio), max(ratio)
+plt.plot(times, [mean/means[-11] for mean in means], label='x = -10')
+plt.ylabel("Temperature ratio")
+plt.xlabel("Time (minutes)")
+ax4 = fig.add_subplot(2, 2, 4)
+plt.title("T(t)/T(t=-30)")
+ratio =	[mean/means[0] for mean in means]
+print min(ratio), max(ratio)
+plt.plot(times, [mean/means[0] for mean in means], label='x = -30')
+plt.xlabel("Time (minutes)")
+#plt.ylim(0, 1)
+#plt.legend()
+plt.savefig("/imaps/holly/home/ajl7/tempplots/tempratios")
+plt.close()"""
+
 # Plot instantaneous temperatures of active regions for all flares against flare class
-fig = plt.figure()
-plt.add_subplot(2, 2, 1)
+fig = plt.figure(figsize=(16, 16))
+ax1 = fig.add_subplot(2, 2, 1)
 plt.title("At time of flare")
-plt.scatter(ar_temps, fl_classes)
+plt.scatter(ar_temps_fltime, fl_classes)
 plt.ylabel("GOES flux of flare")
-plt.add_subplot(2, 2, 2)
+#ax1.set_yscale('log')
+ax2 = fig.add_subplot(2, 2, 2)
 plt.title("1 minute before flare")
-plt.scatter(ar_temps, fl_classes)
-plt.add_subplot(2, 2, 3)
+plt.scatter(ar_temps_1, fl_classes)
+#ax2.set_yscale('log')
+ax3 = fig.add_subplot(2, 2, 3)
 plt.title("10 minutes before flare")
-plt.scatter(ar_temps, fl_classes)
+plt.scatter(ar_temps_10, fl_classes)
 plt.ylabel("GOES flux of flare")
 plt.xlabel("Mean temperature of active region")
-plt.add_subplot(2, 2, 4)
+#ax3.set_yscale('log')
+ax4 = fig.add_subplot(2, 2, 4)
 plt.title("30 minutes before flare")
-plt.scatter(ar_temps, fl_classes)
+plt.scatter(ar_temps_30, fl_classes)
 plt.xlabel("Mean temperature of active region")
+#ax4.set_yscale('log')
 #plt.ylim(0.0, 0.0005)
 plt.savefig("/imaps/holly/home/ajl7/tempplots/allflares")
 plt.close()
